86b55ad558e7f9e733695a2fb43a1faa92d5822e
minheibis <yuukihenmi16@gmail.com>
2021-03-09 07:54:10 +0100

    [add]issue #11 add user to wordpress

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -4,5 +4,6 @@ sed -i 's/skip-networking/#skip-networking/' /etc/my.cnf.d/mariadb-server.cnf
 mysqld -u root &
 sleep 10
 mysql -u root < /tmp/start.sql
+mysql -u root < /tmp/wordpress.sql
 
 telegraf
