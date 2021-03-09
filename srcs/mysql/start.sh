b7fbac210ad4ec2e1d99b00a89a7f91be8db9f5d
minheibis <yuukihenmi16@gmail.com>
2021-03-09 07:10:34 +0100

    [fix]serivce name

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -1,8 +1,8 @@
 mysql_install_db --user=root --datadir=/var/lib/mysql
 sed -i 's/skip-networking/#skip-networking/' /etc/my.cnf.d/mariadb-server.cnf
 
-telegraf &
 mysqld -u root &
 sleep 10
 mysql -u root < /tmp/start.sql
-tail -f /dev/null
+
+telegraf
